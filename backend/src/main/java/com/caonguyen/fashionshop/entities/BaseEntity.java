7976a8a556cd5e1981490e95b51838c7d3758c31
phongnd2802 <duyphong02802@gmail.com>
2024-11-05 09:36:12 +0700

    base entity and verify entity

---
+++ backend/src/main/java/com/caonguyen/fashionshop/entities/BaseEntity.java
@@ -0,0 +1,23 @@
+package com.caonguyen.fashionshop.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@Setter
+public abstract class BaseEntity {
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
--- backend/src/main/java/com/caonguyen/fashionshop/entities/VerifyEntity.java
+++ backend/src/main/java/com/caonguyen/fashionshop/entities/VerifyEntity.java
@@ -1,4 +1,35 @@
 package com.caonguyen.fashionshop.entities;
 
-public class VerifyEntity {
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "verify")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class VerifyEntity extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "verify_email")
+    private String email;
+
+    @Column(name = "verify_otp")
+    private String otp;
+
+    @Column(name = "verify_purpose")
+    private int purpose;
+
+    @Column(name = "is_verified")
+    private boolean verified;
+
+    @Column(name = "is_deleted")
+    private boolean deleted;
 }
