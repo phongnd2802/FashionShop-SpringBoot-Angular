507d917a0826d22256181e2394fb0d1f861e89e0
cdtanhh1807 <cdtanhh@gmail.com>
2024-11-05 10:39:52 +0700

    user entity

--- backend/src/main/java/com/caonguyen/fashionshop/entities/UserEntity.java
+++ backend/src/main/java/com/caonguyen/fashionshop/entities/UserEntity.java
@@ -1,4 +1,35 @@
 package com.caonguyen.fashionshop.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "user")
 public class UserEntity {
+    @Id
+    @Column(name = "user_id", nullable = false)
+    private int id;
+
+    @Column(name = "user_name", length = 50, nullable = false)
+    private String name;
+
+    @Column(name = "user_email", length = 50, nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "user_phoneNumber", length = 10, nullable = true)
+    private String phoneNumber;
+
+    @Column(name = "user_birthDay", nullable = true)
+    private Date birthDay;
+
+    @Column(name = "user_address", length = 300, nullable = false)
+    private String address;
+
+    @Column(name = "user_avatar", length = 300, nullable = true)
+    private String avatar;
 }
